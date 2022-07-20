1f940924f2466ba400578703cd6c730f89efecc1
guzzye13 <eddyguzman322@gmail.com>
2022-07-20 18:30:50 -0400

    updated version

---
+++ variable.py
@@ -0,0 +1,9 @@
+students_count = 1000 # int
+rating = 4.99 # float
+is_published = False # bool
+course_name = "Python programming" # str
+
+print(type(students_count))
+print(type(rating))
+print(type(is_published))
+print(type(course_name))
